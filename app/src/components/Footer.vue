6be72a297269a6b1d546839c501453e021151d42
muniz <daniel.muniz997@gmail.com>
2025-05-09 16:13:30 -0300

    details footer

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -6,6 +6,20 @@
           <ul class="footer-links">
             <li><router-link to="/#">Saúde Longevidade e Autoestima</router-link></li>
           </ul>
+          <h3 class="footer-title">Contato</h3>
+          <ul class="footer-links">
+            <li><router-link to="/#">0800 454 5103</router-link></li>
+            <li><router-link to="/#">1799126-6117 (Whatsapp)</router-link></li>
+          </ul>
+          <h3 class="footer-title">Endereço</h3>
+          <ul class="footer-links">
+            <li><router-link to="/#">Rodovia Fernão Dias, 937</router-link></li>
+            <li><router-link to="/#">Complemento: BW Diase -</router-link></li>
+            <li><router-link to="/#">DocaLog G500 Modulo 5</router-link></li>
+            <li><router-link to="/#">CEP: 37646-202</router-link></li>
+            <li><router-link to="/#">Extrema - MG</router-link></li>
+            <li><router-link to="/#">30.417.094/0001.60</router-link></li>
+          </ul>
         </div>
   
         <div class="footer-section">
